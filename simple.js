de871db361c1eb2a34ab4b20139551f3fb72ea3f
IvanLainez <58564395+IvanLainez@users.noreply.github.com>
2020-01-13 12:11:18 -0500

    Day4

---
+++ simple.js
@@ -0,0 +1,6 @@
+d3.select('p').style("color", 'skyblue');
+d3.select('svg').style("background-color", "skyblue")
+d3.select("rect").style("fill", "red")
+d3.select("h2").style("color", 'red')
+.text("A really simple description")
+d3.select("circle").style("fill", "purple")
